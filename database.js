96f05af47753a3e9ec9399966a208227855a79f5
nattaporn209 <72605645+nattaporn209@users.noreply.github.com>
2021-05-08 00:06:57 +0700

    Delete database.js

--- database.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-const dbConnection = mysql.createPool({
-    host: 'us-cdbr-east-03.cleardb.com', // MYSQL HOST NAME
-    user: 'bcd35e245eea44', // MYSQL USERNAME
-    password: 'ad3d3cbd', // MYSQL PASSWORD
-    database: 'heroku_dcf60f39083f734', // MYSQL DB NAME
-    port: '8889'
-}).promise();
-module.exports = dbConnection;
